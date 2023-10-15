137c47c62a411b3d2242146788ee8c0dc6068f37
Azka Norouzi <azka.norouzi@gmail.com>
2023-10-15 14:48:11 +0330

    ✏️ Implement basic file structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,22 @@
+import { useEffect } from 'react'
+import { themeChange } from 'theme-change'
+/* 
+    <button data-set-theme="night" data-act-class="ACTIVECLASS">
+        b
+      </button>
+      <button data-set-theme="dark" data-act-class="ACTIVECLASS">
+        a
+      </button>
+      <button data-set-theme="light" data-act-class="ACTIVECLASS">
+        s
+      </button>
+*/
 function App() {
-  return <></>
+  // useEffect(() => {
+  //   themeChange(false)
+  //   // 👆 false parameter is required for react project
+  // }, [])
+  return <main></main>
 }
 
 export default App
---
+++ src/components/Main.jsx
@@ -0,0 +1,3 @@
+export default function Main({ children }) {
+  return <main className="grid place-content-center">{children}</main>
+}
